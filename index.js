4c05cec3545b258439186d99fb4a50e37bb25688
kamalabdi8 <kamalabdi042@gmail.com>
2024-09-26 20:03:07 +0300

    scoope

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    customerName = bestCustomer;
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+const leastFavoriteCustomer = 'cindy';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ted';
+    return leastFavoriteCustomer;
+}
